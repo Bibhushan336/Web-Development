cc44696f069cf46a37d3d407272c2f6a5f25e6ff
Bibhushan Thapa <thapabibhushan@gmail.com>
2022-06-23 13:02:22 +0545

    setInterval

---
+++ day18javascript/01.js
@@ -0,0 +1,9 @@
+// set Interval
+
+
+console.log("script start");
+setInterval(() => {
+    // console.log(Math.random());
+    // console.log("bibhusuhan thapa")
+}, 1000);
+console.log("script end");
--- day18javascript/script.js
+++ day18javascript/script.js
@@ -18,7 +18,7 @@ console.log("new code ........................")
 console.log("script start");
 
 const id = setTimeout(() => {
-  console.log("inside setTimeout");
+  console.log("second time out");
 }, 1000);
 
 for (let i = 1; i < 100; i++) {
